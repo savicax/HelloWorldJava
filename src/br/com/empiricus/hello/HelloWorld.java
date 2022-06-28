55953f58f635bd2542e8bdf6c7b66fc261916e18
Savia <saviachristine@gmail.com>
2022-06-28 18:14:48 -0300

    código mais limpo

--- src/br/com/empiricus/hello/HelloWorld.java
+++ src/br/com/empiricus/hello/HelloWorld.java
@@ -5,28 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		//printa no console
 		System.out.println("Hello World!");
-		
-		System.out.println("Trem");
-		
-				//Variaveis --> são posições em memória...
-				//Variaveis --> são tipadas
-				
-				//nomes de variaveis - bom senso
-				//salario_do_funcionario --> não muito utilizado
-				//salFunc --> mais comum..
-				//sf --> não muit utilizado
-				short numero = 128;
-				
-				//byte 1valor;
-				//byte *valor;
-				
-				byte _valor;
-				byte $valor;
-				int nume25ro;
-				
-				//char private;
-				
-				double valorTotal;
 
 	}
 
